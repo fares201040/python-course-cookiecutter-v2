4af127b89e90d2bf2cd1a45e964cb4a023c758de
Faris <fares201040@hotmail.com>
2025-03-03 01:22:34 +0300

    add github token

--- run.sh
+++ run.sh
@@ -215,8 +215,8 @@ function create-sample-repo {
     && git push origin main
 
     gh workflow run .github/workflows/create-or-update-repo.yaml \
-        -f repo_name=generated-repo-28 \
-        -f package_import_name=generated_repo_28 \
+        -f repo_name=generated-repo-26 \
+        -f package_import_name=generated_repo_26 \
         -f is_public_repo=true \
         --ref main
 }
